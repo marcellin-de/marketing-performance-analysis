bab7c555507ef1e1595e177e4e256b49ee2c58f4
marcel <djambomarcellin@gmail.com>
2026-02-07 00:03:53 +0100

    Fix staging UTM normalization and update dbt/Elementary configs and add test for quality

--- dbt_marketing_perf/models/staging/stg_website_sessions.sql
+++ dbt_marketing_perf/models/staging/stg_website_sessions.sql
@@ -1,8 +1,31 @@
 with source as (
     select 
-        website_session_id,created_at,user_id,is_repeat_session,utm_source,utm_campaign,utm_content,device_type,http_referer
+        website_session_id,
+        created_at,
+        user_id,
+        is_repeat_session,
+        utm_source,
+        utm_campaign,
+        utm_content,
+        device_type,
+        http_referer
     from {{source("marketing_perf_source","website_sessions")}}
 )
 
-select * from source
-
+select
+    website_session_id,
+    created_at,
+    user_id,
+    is_repeat_session,
+    case
+        when utm_source is null or utm_source = 'NULL' then 'direct'
+        when utm_source = 'gsearch' then 'google'
+        when utm_source = 'bsearch' then 'bing'
+        when utm_source = 'socialbook' then 'facebook'
+        else 'other'
+    end as utm_source,
+    utm_campaign,
+    utm_content,
+    device_type,
+    http_referer
+from source
